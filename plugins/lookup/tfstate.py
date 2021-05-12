1a062cded744085c0609556344191a13a4e49f19
Norman L. Johnson IV <norman.johnson@sas.com>
2021-05-12 12:25:30 -0400

    workaround for {{EC2PrivateDNSName}} in tfstate (#93)

---
+++ plugins/lookup/tfstate.py
@@ -0,0 +1,21 @@
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+import os
+import subprocess
+import json
+
+
+from ansible.plugins.lookup import LookupBase
+from ansible.module_utils._text import to_native, to_text
+
+class LookupModule(LookupBase):
+  def run(self, terms, variables=None, **kwargs):
+    ret = []
+    for term in terms:
+      tmp_source = self._loader.get_real_file(term)
+      with open(tmp_source, 'r') as jsonfile:
+        jsonfile.seek(0)
+        data = json.load(jsonfile)
+        ret.append(data["outputs"])
+    return ret
