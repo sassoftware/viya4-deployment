cb51edf7bb54ffb85eb630fde63462649efd6657
David Houck <dhoucgitter@users.noreply.github.com>
2023-03-23 15:34:04 -0400

    docs: (IAC-873) Add Copyright information (#406)
    * feat: (IAC-873) Add Copyright information
    
    * feat: (IAC-873) Add copyright to ansible tasks
    
    * feat: (IAC-873) Add copyright to additional ansible files

--- roles/vdm/library/overlay_facts.py
+++ roles/vdm/library/overlay_facts.py
@@ -1,3 +1,7 @@
+#
+# Copyright © 2020-2023, SAS Institute Inc., Cary, NC, USA. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
 from ansible.module_utils.basic import AnsibleModule
 from packaging.version import parse as parse_version
 import os
