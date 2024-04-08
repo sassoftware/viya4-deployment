4b8cd1782082413f73571b439bd5666e6e575982
Ritika Patil <Ritika.Patil@sas.com>
2024-04-08 13:51:50 -0500

    docs: (IAC-1415) Updated copyright year to 2024

--- roles/common/lookup_plugins/tfstate.py
+++ roles/common/lookup_plugins/tfstate.py
@@ -1,5 +1,5 @@
 #
-# Copyright © 2020-2023, SAS Institute Inc., Cary, NC, USA. All Rights Reserved.
+# Copyright © 2020-2024, SAS Institute Inc., Cary, NC, USA. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
 from __future__ import (absolute_import, division, print_function)
