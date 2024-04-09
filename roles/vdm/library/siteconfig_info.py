8453593c1908720b92ba8e9431dbd4fb62e8149a
Ritika Patil <94649368+riragh@users.noreply.github.com>
2024-04-09 11:00:01 -0500

    docs: (IAC-1415) Updated copyright year to 2024 (#540)

--- roles/vdm/library/siteconfig_info.py
+++ roles/vdm/library/siteconfig_info.py
@@ -1,5 +1,5 @@
 #
-# Copyright © 2020-2023, SAS Institute Inc., Cary, NC, USA. All Rights Reserved.
+# Copyright © 2020-2024, SAS Institute Inc., Cary, NC, USA. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
 from ansible.module_utils.basic import *
