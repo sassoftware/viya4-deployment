b1756f93d5ebd42c7f650940edf194df17c8a4b3
Norman Johnson <norman.johnson@sas.com>
2024-08-07 09:54:06 -0400

    fail if user defined overlay is 0

--- roles/vdm/library/siteconfig_info.py
+++ roles/vdm/library/siteconfig_info.py
@@ -17,14 +17,19 @@ class Overlay(Enum):
   TRANSFORMER = "transformers"
 
 class siteConfig(object):
-  def __init__(self, basedir, _overlays):
-    self._overlays = _overlays
+  def __init__(self, basedir, overlays, fail_json):
+    self._overlays = overlays
     self._basedir = os.path.join(basedir, '')
+    self._fail_json = fail_json
 
   def add_overlays(self, overlay_type: Overlay, config: str):
     match = re.search(r"^(\d{1,2})", os.path.basename(config))
-    priority = match.group(1) if match else 50
-
+    priority = 50
+    if match:
+      if int(match.group(1)) == 0:
+        self._fail_json(f"User defined overlay cannot be priority 0, {config}")
+      else:
+        priority = int(match.group(1))
     self._overlays.setdefault(overlay_type.value, {})
     self._overlays[overlay_type.value].setdefault(priority, [])
     self._overlays[overlay_type.value][priority].append(self.remove_basedir(config))
@@ -111,7 +116,7 @@ def main():
   }
   module = AnsibleModule(argument_spec=fields)
   try:
-    sc = siteConfig(module.params['path'], module.params['existing'])
+    sc = siteConfig(module.params['path'], module.params['existing'], module.fail_json)
     scFolder = os.path.join(module.params['path'], 'site-config')
     _, folders, _ = next(os.walk(scFolder))
     for folder in folders:
